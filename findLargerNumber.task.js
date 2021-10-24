35fdd68ba5550f510f99c216f26fc64d8bc196e8
vhorozheiev <vhorozheiev@gmail.com>
2021-10-24 11:46:50 +0300

    Final version of solution

--- findLargerNumber.task.js
+++ findLargerNumber.task.js
@@ -1,13 +1,17 @@
 /* Write a JS program to find if the first number 
 is larger from the two given positive integers */
 
-let firstNumber = 10;
+let firstNumber = 20;
 let secondNumber = 12;
+let thirdNumber = 8;
 
 
-findLargerNumber(firstNumber, secondNumber);
+findLargerNumber(firstNumber, secondNumber, thirdNumber);
 
-function findLargerNumber(firstNumber, secondNumber) {
-    return firstNumber > secondNumber ? true : false;
+function findLargerNumber(firstNumber, secondNumber, thirdNumber) {
+    if (firstNumber > secondNumber && firstNumber > thirdNumber) {
+        return true;
+    }
+    return false;
 };
 
